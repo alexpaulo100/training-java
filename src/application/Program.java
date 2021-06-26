1ea96e57ddb75a6fc1136219656c5a961aa4584a
Alex Silva <alex.paulo100@gmail.com>
2021-06-26 13:41:56 -0300

    Funcionario Lista

---
+++ src/application/Program.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		 
+		System.out.println("Quantos funcionarios serao registrados? ");
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++) {
+			
+			System.out.println();
+			System.out.println("Employee #" + (i + 1) + ":");
+			System.out.println("Id: ");
+			Integer id = sc.nextInt();
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);	
+			
+		}
+		System.out.println();
+		System.out.println("Insira a id do funcionário que terá aumento de salário:  ");
+		int idsalary = sc.nextInt();
+		Integer pos = position(list, idsalary);
+		if (pos == null) {
+			System.out.println("Esse id não existe!");
+		}
+		else {
+			System.out.print("Entre com a porcentagem: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
+		System.out.println();
+		System.out.println("Lista de funcionarios: ");
+		for (Employee emp : list) {
+			System.out.println(emp);
+		}
+		sc.close();
+
+	}
+	public static Integer position(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
+	}
+}
